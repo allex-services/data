39c218e2961a9892f359d5e4c0d4e5cf11b32bce
Andrija Petrovic <andrija.hers@gmail.com>
2015-05-21 18:48:06 +0200

    Introduced better Default handling in Record + proper updates on State=>Data + cleanup

--- data/record/creator.js
+++ data/record/creator.js
@@ -1,22 +1,46 @@
 function createRecord(execlib){
   var lib = execlib.lib;
 
+  function DefaultHandler(desc){
+    this.proc = null;
+    if(lib.isString(desc) && desc.length>4 && desc.indexOf('{{')===0 && desc.lastIndexOf('}}')===desc.length-2){
+      this.proc = function(){
+      };
+    }
+    this._value = desc;
+    if('undefined' === typeof this._value){
+      this._value = null;
+    }
+  }
+  DefaultHandler.prototype.destroy = function(){
+    this.proc = null;
+    this._value = null;
+  };
+  DefaultHandler.prototype.value = function(){
+    if(this.proc){
+      return this.proc();
+    }
+    return this._value;
+  };
+
   function Field(prophash){
     this.name = prophash.name;
     if(!this.name){
       throw "Field needs a name";
     }
     this.value = prophash.value;
-    this.default = prophash.default || null;
+    this.default = new DefaultHandler(prophash.default);
   }
   Field.prototype.destroy = function(){
-    this.name = null;
+    this.default.destroy();
+    this.default = null;
     this.value = null;
+    this.name = null;
   };
   Field.prototype.valueFor = function(val){
     var und;
     if(val===und){
-      return this.default;
+      return this.default.value();
     }
     //TODO: validate
     return val;
@@ -60,86 +84,26 @@ function createRecord(execlib){
       }
     }
   };
-  Record.prototype.stateStreamFilterForRecord = function(storage,datahash){
-    return new StateStreamFilter(storage,datahash,this);
+  Record.prototype.stateStreamFilterForRecord = function(storage,record){
+    return new StateStreamFilter(storage,record,this);
   };
 
-  function StateStreamFilter(manager,datahash,record){
+  function StateStreamFilter(manager,recordinstance,record){
     this.manager = manager;
-    this.hashfilterdescriptor = {op:'hash',d:datahash};
+    this.recordinstance = recordinstance;
     this.record = record;
   }
   StateStreamFilter.prototype.destroy = function(){
-    this.manager = null;
-    this.hashfilterdescriptor = null;
     this.record = null;
+    this.recordinstance = null;
+    this.manager = null;
   };
   StateStreamFilter.prototype.onStream = function(item){
     var val = this.record.filterStateStream(item);
-    //console.log('State=>Data',item,'=>',val,'on',this.record);
     if(val){
-      this.manager.updateByDescriptor(this.hashfilterdescriptor,val);
+      this.manager.updateByDescriptor({op:'hash',d:this.record.filterObject(this.recordinstance)},val);
     }
   };
-
-  /*
-  Record.createFrom = function(prophash){
-    if(!(prophash && prophash.type)){
-      throw "Record needs type in its description";
-    }
-    switch(prophash.type){
-      case 'static':
-        return new StaticPart(prophash);
-      case 'remote':
-        return new RemotePart(prophash);
-      default:
-        throw "RecordPart type "+prophash.type+" not recognized";
-    }
-  };
-
-  function StaticPart(prophash){
-    Part.call(this,prophash);
-  }
-  lib.inherit(StaticPart,Part);
-  StaticPart.prototype.FieldCtor = StaticField;
-  StaticPart.prototype.type = 'static';
-
-  function RemotePart(prophash){
-    Part.call(this,prophash);
-    this.source = prophash.source;
-    this.client = null;//connect to data provider??
-  }
-  lib.inherit(RemotePart,Part);
-  RemotePart.prototype.FieldCtor = RemoteField;
-  RemotePart.prototype.type = 'remote';
-
-
-  function Record(parts){
-    if(!(parts && lib.isArray(parts))){
-      throw "Record needs the recordparts array";
-    }
-    this.length = 0;
-    this.parts = [];
-    this.fields = new lib.Map();
-    parts.forEach(this.addPart.bind(this));
-  }
-  Record.prototype.destroy = function(){
-    this.fields.destroy();
-    this.fields = null;
-    lib.arryDestroyAll(this.parts);
-    this.parts = null;
-    this.length = null;
-  };
-  Record.prototype.addPart = function(partdesc){
-    var rp = Part.createFrom(partdesc);
-    rp.fields.forEach(this.addField.bind(this));
-    this.length += rp.length;
-    this.parts.push(rp);
-  };
-  Record.prototype.addField = function(field){
-    this.fields.add(field.name,field);
-  };
-  */
   return Record;
 }
 
