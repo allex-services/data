2907259f691f01bf6d241a60dd3b6a9cdb472410
Andrija Petrovic <andrija.hers@gmail.com>
2015-05-21 12:26:04 +0200

    Cleaner DataCoder

--- data/codercreator.js
+++ data/codercreator.js
@@ -42,8 +42,8 @@ function createDataCoder(execlib){
     return {
       o: 'u',
       d: {
-        filter:filter.descriptor(),
-        datahash:datahash
+        f:filter.descriptor(),
+        d:datahash
       }
     };
   };
