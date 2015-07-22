05b70cb8652377555c62b10ff150dda6c8b47ce2
Veljko Popovic <veljko@888.rs>
2015-07-22 14:45:38 +0200

    moving along with data

--- web_component/dist/all.js
+++ web_component/dist/all.js
@@ -3,43 +3,55 @@
 //samo da te vidim
 (function (module, lib, allex) {
 
-  module.factory ('allex.data.GridMixIn', ['allex.lib.UserDependentMixIn', function (UserDependentMixIn) {
-    function AllexDataGridMixIn ($scope, gridOptions, subsinkPath) {
+  module.factory('allex.data.DataMonitorMixIn', ['allex.lib.UserDependentMixIn', function (UserDependentMixIn) {
+    function DataMonitorMixIn ($scope, subsinkPath) {
       UserDependentMixIn.call(this, $scope);
       this.subsinkPath = subsinkPath;
-
-      this.gridOptions = gridOptions || {};
-      this.gridOptions.data = "_ctrl.subsink.data";
       this.subsink = null;
-
       this._ad_usr_state_l= this.get('user').attachListener('state', this._ad_usr_stateChanged.bind(this));
     }
-    AllexDataGridMixIn.prototype.__cleanUp = function () {
 
+    DataMonitorMixIn.prototype.__cleanUp = function () {
       this._ad_usr_state_l.destroy();
       this._ad_usr_state_l = null;
-
       this.subsinkPath = null;
-      this.gridOptions = null;
       this.subsink = null;
     };
 
-    AllexDataGridMixIn.prototype._ad_usr_stateChanged = function (state) {
+    DataMonitorMixIn.prototype._ad_usr_stateChanged = function (state) {
       this.set('subsink', this.get('user').getSubSink(this.subsinkPath));
     };
 
-    AllexDataGridMixIn.prototype.set_subsink = function (subsink) {
+    DataMonitorMixIn.prototype.set_subsink = function (subsink) {
       this.subsink = subsink;
       ///hoce li mi ovaj reci destroyed? mislim da ne i da nema potrebe, samo ce da se zanovi valjda
       this.$apply();
     };
 
-    AllexDataGridMixIn.prototype.get_data = function () {
+    DataMonitorMixIn.prototype.get_data = function () {
       return this.subsink ? this.subsink.data : null;
     };
 
+    DataMonitorMixIn.addMethods = function (extendedClass) {
+      lib.inheritMethods (extendedClass, DataMonitorMixIn, 'set_subsink', 'get_data', '_ad_usr_stateChanged');
+    };
+
+    return DataMonitorMixIn;
+  }]);
+
+  module.factory ('allex.data.GridMixIn', ['allex.data.DataMonitorMixIn', function (DataMonitorMixIn) {
+    function AllexDataGridMixIn ($scope, gridOptions, subsinkPath) {
+      this.gridOptions = gridOptions || {};
+      this.gridOptions.data = "_ctrl.subsink.data";
+      DataMonitorMixIn.call(this, $scope, subsinkPath);
+    }
+
+    AllexDataGridMixIn.prototype.__cleanUp = function () {
+      this.gridOptions = null;
+    };
+
     AllexDataGridMixIn.addMethods = function (extendedClass) {
-      lib.inheritMethods (extendedClass, AllexDataGridMixIn, 'set_subsink', 'get_data', '_ad_usr_stateChanged');
+      DataMonitorMixIn.addMethods(extendedClass);
     };
     return AllexDataGridMixIn;
   }]);
