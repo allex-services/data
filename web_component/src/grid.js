51b6aef1f6794d1b8d0bf7ae9abf63f30e13781a
Veljko Popovic <veljko@888.rs>
2015-07-28 10:26:17 +0200

    major bug with scope data reference

--- web_component/src/grid.js
+++ web_component/src/grid.js
@@ -1,44 +1,5 @@
 (function (module, lib, allex) {
 
-  module.factory('allex.data.DataMonitorMixIn', ['allex.lib.UserDependentMixIn', function (UserDependentMixIn) {
-    function DataMonitorMixIn ($scope, subsinkPath) {
-      UserDependentMixIn.call(this, $scope);
-      this.subsinkPath = subsinkPath;
-      this.data = null;
-      this._ad_usr_state_l= this.get('user').attachListener('state', this._ad_usr_stateChanged.bind(this));
-      this._mgl = null;
-    }
-
-    DataMonitorMixIn.prototype.__cleanUp = function () {
-      this._ad_usr_state_l.destroy();
-      this._ad_usr_state_l = null;
-      this.subsinkPath = null;
-      this.data = null;
-      this._mgl.destroy();
-      this._mgl = null;
-    };
-
-    DataMonitorMixIn.prototype._ad_usr_stateChanged = function (state) {
-      this.set_subsink(this.get('user').getSubSink(this.subsinkPath));
-    };
-
-    DataMonitorMixIn.prototype.set_subsink = function (subsink) {
-      if (subsink) {
-        this.data = subsink.data;
-        subsink.monitorDataForGui(this.$apply.bind(this));
-      }else{
-        this.data = null;
-      }
-      this.$apply();
-    };
-
-    DataMonitorMixIn.addMethods = function (extendedClass) {
-      lib.inheritMethods (extendedClass, DataMonitorMixIn, 'set_subsink', 'get_data', '_ad_usr_stateChanged');
-    };
-
-    return DataMonitorMixIn;
-  }]);
-
   module.factory ('allex.data.GridMixIn', ['allex.data.DataMonitorMixIn', function (DataMonitorMixIn) {
 
     var DEFAULT_GRID_OPTIONS = {
--- web_component/src/index.js
+++ web_component/src/index.js
@@ -1,2 +1,46 @@
 (function (module, lib, allex) {
+  module.factory('allex.data.DataMonitorMixIn', ['allex.lib.UserDependentMixIn', function (UserDependentMixIn) {
+    function DataMonitorMixIn ($scope, subsinkPath) {
+      UserDependentMixIn.call(this, $scope);
+      this.subsinkPath = subsinkPath;
+      this.data = null;
+      this._ad_usr_state_l= this.get('user').attachListener('state', this._ad_usr_stateChanged.bind(this));
+      this._mgl = null;
+    }
+
+    DataMonitorMixIn.prototype.__cleanUp = function () {
+      this._ad_usr_state_l.destroy();
+      this._ad_usr_state_l = null;
+      this.subsinkPath = null;
+      this.data = null;
+      if (this._mgl) this._mgl.destroy();
+      this._mgl = null;
+    };
+
+    DataMonitorMixIn.prototype._ad_usr_stateChanged = function (state) {
+      this.set_subsink(this.get('user').getSubSink(this.subsinkPath));
+    };
+
+    DataMonitorMixIn.prototype.set_subsink = function (subsink) {
+      if (subsink) {
+        this.data = subsink.data;
+        this._mgl = subsink.monitorDataForGui(this.$apply.bind(this));
+      }
+
+      /*
+      DONT YOU EVER AGAIN DO SOMETHING LIKE THIS ... changing reference in the scope on the fly was neve a good thing ...
+      else{
+        this.data = null;
+      }
+      */
+      this.$apply();
+    };
+
+    DataMonitorMixIn.addMethods = function (extendedClass) {
+      lib.inheritMethods (extendedClass, DataMonitorMixIn, 'set_subsink', 'get_data', '_ad_usr_stateChanged');
+    };
+
+    return DataMonitorMixIn;
+  }]);
+
 })(angular.module('allex.data', ['allex.lib', 'ui.grid','ui.grid.autoResize']), ALLEX.lib, ALLEX);
