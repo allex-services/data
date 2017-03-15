54975ac86703f30c7fab98bf1f27336c2267ab37
Andrija Petrovic <andrija.hers@gmail.com>
2017-03-15 10:34:26 +0000

    aggregators introduced

--- servicecreator.js
+++ servicecreator.js
@@ -22,10 +22,67 @@ function createDataService(execlib, ParentService, datafilterslib){
     };
   }
 
+  function Aggregator (onDataReady, storageinstance, config) {
+    this._rn = null;
+    this._aggregate = storageinstance.aggregate.bind(storageinstance, config.query);
+    this._interval = config.interval;
+    this.onDataReady = onDataReady;
+    this.data = null;
+    this._go();
+  }
+
+  Aggregator.prototype.destroy = function () {
+    if (this._rn) this._rn.destroy();
+    this._rn = null;
+    this._interval = null;
+    this._aggregate = null;
+    this.onDataReady = null;
+    this.data = null;
+  };
+
+  Aggregator.prototype._go = function () {
+    if (this._rn) this._rn.destroy();
+    this._rn = null;
+
+    this._aggregate().done (this._onSuccess.bind(this), this._onFailed.bind(this), this._onProgress.bind(this));
+  };
+
+  Aggregator.prototype._next = function () {
+    this._rn = lib.runNext (this._go.bind(this), this._interval);
+  };
+
+  Aggregator.prototype._onSuccess = function () {
+    this.onDataReady(this.data);
+    this._next();
+  };
+
+  Aggregator.prototype._onProgress = function (aggdata) {
+    if ('start' === aggdata.op) {
+      this.data = [];
+      return;
+    }
+
+    if ('next' === aggdata.op) {
+      this.data.push (aggdata.data);
+      return;
+    }
+  };
+
+  Aggregator.prototype._onFailed = function (err) {
+    if (err.code === 'NOT_CONNECTED'){
+      lib.runNext (this._go.bind(this), 100);
+      return;
+    }
+
+    console.log('FAILED TO AGGREGATE', err);
+    this.next();
+  };
+
   function DataService(prophash){
     ParentService.call(this,prophash);
     this.intermediateStorage = null;
     this.data = null;
+    this._aggregations = null;
     this.createStorageAsync(prophash).done(
       this.createData.bind(this, prophash)
     );
@@ -38,6 +95,11 @@ function createDataService(execlib, ParentService, datafilterslib){
     childCtor.prototype.storageDescriptor = dataSuite.inherit(this.prototype.storageDescriptor,childStorageDescriptor);
   };
   DataService.prototype.__cleanUp = function () {
+    if (this._aggregations) {
+      lib.containerDestroyAll (this._aggregations);
+      this._aggregations.destroy();
+    }
+    this._aggregations = null;
     if(this.data){
       this.data.destroy();
     }
@@ -68,7 +130,24 @@ function createDataService(execlib, ParentService, datafilterslib){
     if (this.readyToAcceptUsersDefer) {
       this.readyToAcceptUsersDefer.resolve(true);
     }
+    if (!prophash.storage || !prophash.storage.aggregations) return; //nothing to be done ...
+    
+    this._aggregations = new lib.Map();
+    lib.traverseShallow (prophash.storage.aggregations, this._createAggregation.bind(this, storageinstance));
   };
+
+  DataService.prototype._createAggregation = function (storageinstance, item, alias) {
+    this._aggregations.add (alias, new Aggregator(this._onAggregationDataReady.bind(this, alias), storageinstance, item));
+  };
+
+  DataService.prototype._getAggregationStateNameFromAlias = function (alias) {
+    return 'aggregation_'+alias;
+  };
+
+  DataService.prototype._onAggregationDataReady = function (alias, data) {
+    this.state.set(this._getAggregationStateNameFromAlias (alias), data);
+  };
+
   DataService.prototype.createStorageAsync = function (prophash){
     var d;
     if(prophash && prophash.storage && prophash.storage.modulename){
