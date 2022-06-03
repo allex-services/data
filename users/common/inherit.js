d0d411f78d94a27637693263bb58dd0f7580b7c0
Andrija Petrovic <andrija.hers@gmail.com>
2022-06-03 12:56:47 +0200

    More care in basic inherited methods for Users

--- users/common/inherit.js
+++ users/common/inherit.js
@@ -18,6 +18,14 @@ function commonInherit(execlib,ChildClass,ParentClass,methoddescriptors,userSess
   };
   ChildClass.prototype.read = function (querydescriptor, defer) {
     var query;
+    if (!this.__service) {
+      defer.reject(new lib.Error('ALREADY_DESTROYED'));
+      return;
+    }
+    if (!(this.__service.data && this.__service.storageDescriptor && this.__service.storageDescriptor.record)) {
+      defer.reject(new lib.Error('SERVICE_ALREADY_DESTROYED'));
+      return;
+    }
     dataSuite.fixvisiblefields(querydescriptor, this.visibleFields);
     query = new dataSuite.StaticQuery(this.__service.storageDescriptor.record, querydescriptor);
     defer.promise.then(
@@ -27,13 +35,37 @@ function commonInherit(execlib,ChildClass,ParentClass,methoddescriptors,userSess
     this.__service.data.read(query, defer);
   };
   ChildClass.prototype.update = function(filterdescriptor,datahash,options,defer){
+    if (!this.__service) {
+      defer.reject(new lib.Error('ALREADY_DESTROYED'));
+      return;
+    }
+    if (!this.__service.data) {
+      defer.reject(new lib.Error('SERVICE_ALREADY_DESTROYED'));
+      return;
+    }
     this.__service.data.update(filterdescriptor,datahash,options,defer);
   };
   ChildClass.prototype.delete = function(filterdescriptor,defer){
+    if (!this.__service) {
+      defer.reject(new lib.Error('ALREADY_DESTROYED'));
+      return;
+    }
+    if (!this.__service.data) {
+      defer.reject(new lib.Error('SERVICE_ALREADY_DESTROYED'));
+      return;
+    }
     this.__service.data.delete(filterdescriptor, defer);
   };
 
   ChildClass.prototype.aggregate = function (aggregation_descriptor, defer){
+    if (!this.__service) {
+      defer.reject(new lib.Error('ALREADY_DESTROYED'));
+      return;
+    }
+    if (!this.__service.data) {
+      defer.reject(new lib.Error('SERVICE_ALREADY_DESTROYED'));
+      return;
+    }
     this.__service.data.aggregate (aggregation_descriptor, defer);
   };
   ChildClass.prototype.getSessionCtor = userSessionFactory;
